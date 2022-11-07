6bee9e838e838aeb9843fbef6df25f56630db167
Dejan Knezevic <mail@dknezevic.dev>
2022-11-07 02:04:28 +0100

    Improve explore route and remove paths from db

--- videoshare/api/explore.py
+++ videoshare/api/explore.py
@@ -1,27 +1,57 @@
+from operator import and_, or_
 from typing import Any
 
 from flask import Blueprint
+from sqlalchemy import select
 
-from videoshare.errors import BadRequest
-from videoshare.models import Folder
+from videoshare.errors import NotFound
+from videoshare.models import Folder, Node
 
 explore_blueprint = Blueprint("explore", __name__, url_prefix="/explore")
 
 
+@explore_blueprint.route("/")
 @explore_blueprint.route("/<path:path>")
-def resolve_path(path: str) -> dict[str, Any]:
+def resolve_path(path: str | None = None) -> dict[str, Any]:
     """Resolve path and return list of folders from root to parent"""
-
     if not path:
-        raise BadRequest("Invalid path provided")
+        # As path is not specified, using root
+        nodes = Node.query.filter(Node.parent_id.is_(None)).all()
+    else:
+        path_names = path.rstrip("/").split("/")
+        root_folder = path_names[0]
+        subquery = select(Folder.id).filter(Folder.name == root_folder).subquery()
+        for name in path_names[1:]:
+            subquery = (
+                select(Folder.id)
+                .filter(
+                    or_(
+                        # Get next folder
+                        and_(
+                            Folder.parent_id.in_(select(subquery)), Folder.name == name
+                        ),
+                        # And keep current folder
+                        Folder.id.in_(select(subquery)),
+                    ),
+                )
+                .subquery()
+            )
+        folders = Folder.query.filter(Folder.id.in_(select(subquery))).all()
 
-    folder = Folder.query.filter_by(path=path).first()
-    if not folder:
-        raise BadRequest("Path is not a folder or does not exist")
+        if not folders or len(folders) != len(path_names):
+            raise NotFound("Path could not be resolved")
+
+        nodes = Node.query.filter(Node.parent_id == folders[-1].id).all()
 
     return {
-        "id": folder.id,
-        "name": folder.name,
-        "parent_id": folder.parent_id,
-        "type": folder.type,
+        "path": path,
+        "data": [
+            {
+                "id": node.id,
+                "name": node.name,
+                "type": node.type,
+                "parent_id": node.parent_id,
+            }
+            for node in nodes
+        ],
     }
